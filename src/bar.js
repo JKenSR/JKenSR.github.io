b0e6fac38bd207536e1294db6660914ac8db6ad8
JKenSR <justkensosa@gmail.com>
2026-03-18 15:57:48 -0600

    Top bar

--- src/bar.js
+++ src/bar.js
@@ -18,3 +18,16 @@ document.addEventListener("click", function(e) {
 //     );
 //   }
 // });
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const btn = document.getElementById('btn');
+    sidebar.classList.toggle('close');
+    if (sidebar.classList.contains('close')) {
+        btn.classList.remove('fa-chevron-up');
+        btn.classList.add('fa-chevron-down');
+    } else {
+        btn.classList.remove('fa-chevron-down');
+        btn.classList.add('fa-chevron-up');
+    }
+}
